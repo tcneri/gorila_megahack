2d55f0a90b5a998f303e5049fcef49d99e643a03
Erik Ferreira <ekdespe@gmail.com>
2020-05-04 06:55:48 +0100

    add requirements.txt

---
+++ app.py
@@ -0,0 +1,76 @@
+import requests
+from flask import Flask
+from flask_restful import Resource, Api , reqparse
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('q', action= 'append')
+parser.add_argument('id')
+
+perf = ['conservador', 'moderado', 'arrojado']
+quest = {
+        0:{'a':0,'b':2,'c':3,'d':4},
+        1:{'a':0,'b':2,'c':4,'d':5},
+        2:{'a':0,'b':2,'c':4},
+        3:{'a':0,'b':1,'c':2,'d':4},
+        4:{'a':0,'b':2,'c':4},
+        5:{'a':0,'b':2,'c':4},
+        6:{'a':0,'b':2,'c':3,'d':4},
+        7:{'a':0,'b':2,'c':3,'d':4}
+
+        }
+
+alternativs = ['a','b','c','d']
+
+class ClientProfile(Resource):
+
+    
+    def post(self):
+        args = parser.parse_args()
+        alt = args['q']
+        soma = 0
+        for i in range (0,len(quest)):
+            if alt[i] not in alternativs:
+                erro = 1
+                break
+            else:
+                erro = 0
+                soma += quest[i][alt[i]]
+
+        if erro == 0:
+            
+            if soma <= 9:
+                resposta = perf[0]
+            
+            elif soma > 9 and soma <= 25:    
+                resposta = perf[1]
+            
+            else:
+                resposta = perf[2]
+
+           # payload = {'id': args['id'], 'perfil': resposta}
+           # r = requests.post('/cliente/set/perfil', data = payload)
+
+        else:
+            resposta = 'Entrada inválida'
+
+        #if(r.status_code == 200):
+        #    status = 'Sucesso'
+        #else:
+        #    status = 'Falha'
+        status = 'Falha'
+
+        return {"Inserção_banco" : status, "Perfil": resposta } , 201
+
+   
+api.add_resource(ClientProfile,
+        '/')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=False)
